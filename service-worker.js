aa61e82b12009cd7a6fde5069f3566fc257034a1
sufian62671 <abusufian626710@gmail.com>
2025-07-06 09:14:51 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,43 +1,34 @@
-const CACHE_NAME = "hishab-cache-v1";
+const CACHE_NAME = "hisebkhata-cache-v1";
 const urlsToCache = [
-  "/hisebkhata009/",
-  "/hisebkhata009/index.html",
-  "/hisebkhata009/style.css",
-  "/hisebkhata009/app.js",
-  "/hisebkhata009/manifest.json",
-  "/hisebkhata009/icons/icon-192.png",
-  "/hisebkhata009/icons/icon-512.png"
+  "/Hisebkhata/",
+  "/Hisebkhata/index.html",
+  "/Hisebkhata/style.css",
+  "/Hisebkhata/app.js",
+  "/Hisebkhata/manifest.json",
+  "/Hisebkhata/icons/icon-192.png",
+  "/Hisebkhata/icons/icon-512.png"
 ];
 
-// Install event
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-// Activate event
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(cacheNames =>
-      Promise.all(
-        cacheNames.map(name => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
-          }
-        })
-      )
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      }))
     )
   );
 });
 
-// Fetch event
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request).catch(() => {
-        return caches.match("/hisebkhata009/index.html");
-      });
-    })
+    caches.match(event.request).then(res =>
+      res || fetch(event.request).catch(() => caches.match("/Hisebkhata/index.html"))
+    )
   );
 });
